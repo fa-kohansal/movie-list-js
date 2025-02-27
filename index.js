9180c2156c3e4117192b045ea7822c3e9edb97d9
fa-kohansal <farnaz_kohansal@yahoo.com>
2025-02-26 22:15:17 -0800

    complete filter section and functions

--- index.js
+++ index.js
@@ -3,20 +3,40 @@ let movieList = document.querySelector("ul");
 let filterBtn= document.querySelector("#filterBtn")
 let filterSec = document.querySelector("#filterSec");
 let closeBtn = document.querySelector(".bi-x-square-fill");
+let filterInput = document.querySelector("#filterInput");
+let li = document.createElement("li");
+let myMovie=[]
 let clearInput = () => {
     inp.value = ""; 
 }
 
 function addMovie() {
     let userTypedText = inp.value;
-    let li = document.createElement("li");
-    let textToInsert = document.createTextNode(userTypedText);
-    li.appendChild(textToInsert);
-    movieList.appendChild(li);
-    console.log(movieList);
+    if(userTypedText.trim()===""){
+        alert("please enter a movie name")
+    }
+    else{
+        myMovie.push(userTypedText);
+        console.log(myMovie);
+        // myMovie.forEach((movie) => {
+        //     li.textContent = movie;
+        //     movieList.appendChild(li);
+        // });
+        displayMovies(myMovie)
+        clearInput();
+
+    }
     
-    clearInput();
 }
+function displayMovies(movies){
+    movieList.innerHTML= "";
+    movies.forEach((movie)=>{
+        let li = document.createElement("li");
+        li.textContent = movie;
+        movieList.appendChild(li);
+    })
+}
+
 filterBtn.addEventListener("click",()=>{
     console.log("ok");
     
@@ -26,4 +46,11 @@ filterBtn.addEventListener("click",()=>{
 closeBtn.addEventListener("click",()=>{
     filterSec.classList.add("hiddenFilterSec");
     filterSec.classList.remove("filterSec");
+        displayMovies(myMovie);
+
 })
+function filterItems(){
+    let filterText = filterInput.value.toLowerCase();
+    let filteredMovie= myMovie.filter(movie=>movie.toLowerCase().includes(filterText));
+    displayMovies(filteredMovie)
+}
